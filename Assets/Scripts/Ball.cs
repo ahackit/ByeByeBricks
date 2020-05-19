67c6117b5bd414424b7273ed984d9f3950c4d526
Austin Hackett <ahackit.gmail.com>
2020-05-19 07:43:05 -0500

    Tunes. Adds deploy

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -79,7 +79,6 @@ public class Ball : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
 
-        Debug.Log(myVelocity);
         if (fire)
         {
             if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Bricks"))
